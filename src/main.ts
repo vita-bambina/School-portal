4e24621254badd523bf3d8d84b0a501500089592
Blessing <bambinablessing2214@gmail.com>
2026-07-30 15:59:42 +0100

    Asoirant module added and some few things in enrollment side

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -16,6 +15,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: ' http://localhost:5173/',
+    credentials: true,
+  });
 
   await app.listen(8000);
 }
