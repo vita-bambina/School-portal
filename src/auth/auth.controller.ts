b9d377ff86ec179a338cdbab8ae3d6edfcd1851e
Blessing <bambinablessing2214@gmail.com>
2026-09-02 18:44:25 +0100

    made small chage

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,7 +26,7 @@ export class AuthController {
     res.cookie('token', result.token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
